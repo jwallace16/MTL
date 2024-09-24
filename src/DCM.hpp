c5776aa2b5a254fe0d782cf4ff29df4073596d51
David W <jamesdwallace55@gmail.com>
2024-09-24 10:04:25 -0600

    Added initializer list ctor and rearranged euler angle to dcm conversion to match textbook

--- src/DCM.hpp
+++ src/DCM.hpp
@@ -9,6 +9,8 @@
 #ifndef _DCM_HPP__
 #define _DCM_HPP__
 
+#include <initializer_list>
+
 #include "Matrix.hpp"
 #include "SquareMatrix.hpp"
 #include "Quaternion.hpp"
@@ -36,6 +38,9 @@ public:
 
     //! Construct from a flat array of data
     explicit DCM(const T values[9]);
+
+    //! Construct using initializer list
+    DCM(std::initializer_list<std::initializer_list<T>> list);
 
     //! Copy constructor from Matrix<T, 3, 3> type
     DCM(const Matrix<T, 3, 3> &other);
@@ -68,6 +73,13 @@ DCM<T>::DCM(const T values[3][3]):
 template<class T>
 DCM<T>::DCM(const T values[9]):
     SquareMatrix<T, 3>(values)
+{
+}
+
+//! Construct using initializer list
+template<class T>
+DCM<T>::DCM(std::initializer_list<std::initializer_list<T>> list):
+    SquareMatrix<T, 3>(list)
 {
 }
 
@@ -105,6 +117,7 @@ DCM<T>::DCM(const Quaternion<T> &q)
 template<class T>
 DCM<T>::DCM(const Euler<T> &e)
 {
+    // Active rotations
     T a1 = e.getAngle1();
     T a2 = e.getAngle2();
     T a3 = e.getAngle3();
@@ -112,8 +125,8 @@ DCM<T>::DCM(const Euler<T> &e)
     {
         case RotationSequence::ZXZ_313:
         {
-            this->data[0] = std::cos(a1)*std::cos(a3) - std::cos(a2)*std::sin(a1)*std::sin(a3);
-            this->data[1] = -std::cos(a1)*std::sin(a3) - std::cos(a2)*std::cos(a3)*std::sin(a1);
+            this->data[0] = -std::sin(a1)*std::cos(a2)*std::sin(a3) + std::cos(a1)*std::cos(a3);
+            this->data[1] = -std::sin(a1)*std::cos(a2)*std::cos(a3) - std::cos(a1)*std::sin(a3);
             this->data[2] = std::sin(a1)*std::sin(a2);
             this->data[3] = std::cos(a3)*std::sin(a1) + std::cos(a1)*std::cos(a2)*std::sin(a3);
             this->data[4] = std::cos(a1)*std::cos(a2)*std::cos(a3) - std::sin(a1)*std::sin(a3);
@@ -127,37 +140,37 @@ DCM<T>::DCM(const Euler<T> &e)
         {
             this->data[0] = std::cos(a2);
             this->data[1] = std::sin(a2)*std::sin(a3);
-            this->data[2] = std::cos(a3)*std::sin(a2);
+            this->data[2] = std::sin(a2)*std::cos(a3);
             this->data[3] = std::sin(a1)*std::sin(a2);
-            this->data[4] = std::cos(a1)*std::cos(a3) - std::cos(a2)*std::sin(a1)*std::sin(a3);
-            this->data[5] = -std::cos(a1)*std::sin(a3) - std::cos(a2)*std::cos(a3)*std::sin(a1);
+            this->data[4] = std::cos(a1)*std::cos(a3) - std::sin(a1)*std::cos(a2)*std::sin(a3);
+            this->data[5] = -std::cos(a1)*std::sin(a3) - std::sin(a1)*std::cos(a2)*std::cos(a3);
             this->data[6] = -std::cos(a1)*std::sin(a2);
-            this->data[7] = std::cos(a3)*std::sin(a1) + std::cos(a1)*std::cos(a2)*std::sin(a3);
-            this->data[8] = std::cos(a1)*std::cos(a2)*std::cos(a3) - std::sin(a1)*std::sin(a3);
+            this->data[7] = std::sin(a1)*std::cos(a3) + std::cos(a1)*std::cos(a2)*std::sin(a3);
+            this->data[8] = -std::sin(a1)*std::sin(a3) + std::cos(a1)*std::cos(a2)*std::cos(a3);
         } break;
 
         case RotationSequence::YZY_232:
         {
             this->data[0] = std::cos(a1)*std::cos(a2)*std::cos(a3) - std::sin(a1)*std::sin(a3);
-            this->data[1] = -std::cos(a1)*std::sin(a1);
-            this->data[2] = std::cos(a3)*std::sin(a1) + std::cos(a1)*std::cos(a2)*std::sin(a3);
-            this->data[3] = std::cos(a3)*std::sin(a2);
+            this->data[1] = -std::cos(a1)*std::sin(a2);
+            this->data[2] = std::cos(a1)*std::cos(a2)*std::sin(a3) + std::sin(a1)*std::cos(a3);
+            this->data[3] = std::sin(a2)*std::cos(a3);
             this->data[4] = std::cos(a2);
             this->data[5] = std::sin(a2)*std::sin(a3);
-            this->data[6] = -std::cos(a1)*std::sin(a3) - std::cos(a2)*std::cos(a3)*std::sin(a1);
+            this->data[6] = -std::sin(a1)*std::cos(a2)*std::cos(a3) - std::cos(a1)*std::sin(a3);
             this->data[7] = std::sin(a1)*std::sin(a2);
-            this->data[8] = std::cos(a1)*std::cos(a3) - std::cos(a2)*std::sin(a1)*std::sin(a3);
+            this->data[8] = -std::sin(a1)*std::cos(a2)*std::sin(a3) + std::cos(a1)*std::cos(a3);
         } break;
 
         case RotationSequence::ZYZ_323:
         {
             this->data[0] = std::cos(a1)*std::cos(a2)*std::cos(a3) - std::sin(a1)*std::sin(a3);
-            this->data[1] = -std::cos(a3)*std::sin(a1) - std::cos(a1)*std::cos(a2)*std::sin(a3);
+            this->data[1] = -std::cos(a1)*std::cos(a2)*std::sin(a3) - std::sin(a1)*std::cos(a3);
             this->data[2] = std::cos(a1)*std::sin(a2);
-            this->data[3] = std::cos(a1)*std::sin(a3) + std::cos(a2)*std::cos(a3)*std::sin(a1);
-            this->data[4] = std::cos(a1)*std::cos(a3) - std::cos(a2)*std::sin(a1)*std::sin(a3);
+            this->data[3] = std::sin(a1)*std::cos(a2)*std::cos(a3) + std::cos(a1)*std::sin(a3);
+            this->data[4] = -std::sin(a1)*std::cos(a2)*std::sin(a3) + std::cos(a1)*std::cos(a3);
             this->data[5] = std::sin(a1)*std::sin(a2);
-            this->data[6] = -std::cos(a3)*std::sin(a2);
+            this->data[6] = -std::sin(a2)*std::cos(a3);
             this->data[7] = std::sin(a2)*std::sin(a3);
             this->data[8] = std::cos(a2);
         } break;
@@ -165,25 +178,25 @@ DCM<T>::DCM(const Euler<T> &e)
         case RotationSequence::XZX_131:
         {
             this->data[0] = std::cos(a2);
-            this->data[1] = -std::cos(a3)*std::sin(a2);
+            this->data[1] = -std::sin(a2)*std::cos(a3);
             this->data[2] = std::sin(a2)*std::sin(a3);
             this->data[3] = std::cos(a1)*std::sin(a2);
             this->data[4] = std::cos(a1)*std::cos(a2)*std::cos(a3) - std::sin(a1)*std::sin(a3);
-            this->data[5] = -std::cos(a3)*std::sin(a1) - std::cos(a1)*std::cos(a2)*std::sin(a3);
+            this->data[5] = -std::cos(a1)*std::cos(a2)*std::sin(a3) - std::sin(a1)*std::cos(a3);
             this->data[6] = std::sin(a1)*std::sin(a2);
-            this->data[7] = std::cos(a1)*std::sin(a3) + std::cos(a2)*std::cos(a3)*std::sin(a1);
-            this->data[8] = std::cos(a1)*std::cos(a3) - std::cos(a2)*std::sin(a1)*std::sin(a3);
+            this->data[7] = std::sin(a1)*std::cos(a2)*std::cos(a3) + std::cos(a1)*std::sin(a3);
+            this->data[8] = -std::sin(a1)*std::cos(a2)*std::sin(a3) + std::cos(a1)*std::cos(a3);
         } break;
 
         case RotationSequence::YXY_212:
         {
-            this->data[0] = std::cos(a1)*std::cos(a3) - std::cos(a2)*std::sin(a1)*std::sin(a3);
+            this->data[0] = -std::sin(a1)*std::cos(a2)*std::sin(a3) + std::cos(a1)*std::cos(a3);
             this->data[1] = std::sin(a1)*std::sin(a2);
-            this->data[2] = std::cos(a1)*std::sin(a3) + std::cos(a2)*std::cos(a3)*std::sin(a1);
+            this->data[2] = std::sin(a1)*std::cos(a2)*std::cos(a3) + std::cos(a1)*std::sin(a3);
             this->data[3] = std::sin(a2)*std::sin(a3);
             this->data[4] = std::cos(a2);
-            this->data[5] = -std::cos(a3)*std::sin(a2);
-            this->data[6] = -std::cos(a3)*std::sin(a1) - std::cos(a1)*std::cos(a2)*std::sin(a3);
+            this->data[5] = -std::sin(a2)*std::cos(a3);
+            this->data[6] = -std::cos(a1)*std::cos(a2)*std::sin(a3) - std::sin(a1)*std::cos(a3);
             this->data[7] = std::cos(a1)*std::sin(a2);
             this->data[8] = std::cos(a1)*std::cos(a2)*std::cos(a3) - std::sin(a1)*std::sin(a3);
         } break;
@@ -193,35 +206,35 @@ DCM<T>::DCM(const Euler<T> &e)
             this->data[0] = std::cos(a2)*std::cos(a3);
             this->data[1] = -std::cos(a2)*std::sin(a3);
             this->data[2] = std::sin(a2);
-            this->data[3] = std::cos(a1)*std::sin(a3) + std::cos(a3)*std::sin(a1)*std::sin(a2);
-            this->data[4] = std::cos(a1)*std::cos(a3) - std::sin(a1)*std::sin(a2)*std::sin(a3);
-            this->data[5] = -std::cos(a2)*std::sin(a1);
-            this->data[6] = std::sin(a1)*std::sin(a3) - std::cos(a1)*std::cos(a3)*std::sin(a2);
-            this->data[7] = std::cos(a3)*std::sin(a1) + std::cos(a1)*std::sin(a2)*std::sin(a3);
+            this->data[3] = std::sin(a1)*std::sin(a2)*std::cos(a3) + std::cos(a1)*std::sin(a3);
+            this->data[4] = -std::sin(a1)*std::sin(a2)*std::sin(a3) + std::cos(a1)*std::cos(a3);
+            this->data[5] = -std::sin(a1)*std::cos(a2);
+            this->data[6] = -std::cos(a1)*std::sin(a2)*std::cos(a3) + std::sin(a1)*std::sin(a3);
+            this->data[7] = std::cos(a1)*std::sin(a2)*std::sin(a3) + std::sin(a1)*std::cos(a3);
             this->data[8] = std::cos(a1)*std::cos(a2);
         } break;
 
         case RotationSequence::YZX_231:
         {
             this->data[0] = std::cos(a1)*std::cos(a2);
-            this->data[1] = std::sin(a1)*std::sin(a3) - std::cos(a1)*std::cos(a3)*std::sin(a2);
-            this->data[2] = std::cos(a3)*std::sin(a1) + std::cos(a1)*std::sin(a2)*std::sin(a3);
+            this->data[1] = -std::cos(a1)*std::sin(a2)*std::cos(a3) + std::sin(a1)*std::sin(a3);
+            this->data[2] = std::cos(a1)*std::sin(a2)*std::sin(a3) + std::sin(a1)*std::cos(a3);
             this->data[3] = std::sin(a2);
             this->data[4] = std::cos(a2)*std::cos(a3);
             this->data[5] = -std::cos(a2)*std::sin(a3);
-            this->data[6] = -std::cos(a2)*std::sin(a1);
-            this->data[7] = std::cos(a1)*std::sin(a3) + std::cos(a3)*std::sin(a1)*std::sin(a2);
-            this->data[8] = std::cos(a1)*std::cos(a3) - std::sin(a1)*std::sin(a2)*std::sin(a3);
+            this->data[6] = -std::sin(a1)*std::cos(a2);
+            this->data[7] = std::sin(a1)*std::sin(a2)*std::cos(a3) + std::cos(a1)*std::sin(a3);
+            this->data[8] = -std::sin(a1)*std::sin(a2)*std::sin(a3) + std::cos(a1)*std::cos(a3) ;
         } break;
 
         case RotationSequence::ZXY_312:
         {
-            this->data[0] = std::cos(a1)*std::cos(a3) - std::sin(a1)*std::sin(a2)*std::sin(a3);
-            this->data[1] = -std::cos(a2)*std::sin(a2);
-            this->data[2] = std::cos(a1)*std::sin(a3) + std::cos(a3)*std::sin(a1)*std::sin(a2);
-            this->data[3] = std::cos(a3)*std::sin(a1) + std::cos(a1)*std::sin(a2)*std::sin(a3);
+            this->data[0] = -std::sin(a1)*std::sin(a2)*std::sin(a3) + std::cos(a1)*std::cos(a3);
+            this->data[1] = -std::sin(a1)*std::cos(a2);
+            this->data[2] = std::sin(a1)*std::sin(a2)*std::cos(a3) + std::cos(a1)*std::sin(a3);
+            this->data[3] = std::cos(a1)*std::sin(a2)*std::sin(a3) + std::sin(a1)*std::cos(a3);
             this->data[4] = std::cos(a1)*std::cos(a2);
-            this->data[5] = std::sin(a1)*std::sin(a3) - std::cos(a1)*std::cos(a3)*std::sin(a2);
+            this->data[5] = -std::cos(a1)*std::sin(a2)*std::cos(a3) + std::sin(a1)*std::sin(a3);
             this->data[6] = -std::cos(a2)*std::sin(a3);
             this->data[7] = std::sin(a2);
             this->data[8] = std::cos(a2)*std::cos(a3);
@@ -232,22 +245,22 @@ DCM<T>::DCM(const Euler<T> &e)
             this->data[0] = std::cos(a2)*std::cos(a3);
             this->data[1] = -std::sin(a2);
             this->data[2] = std::cos(a2)*std::sin(a3);
-            this->data[3] = std::sin(a1)*std::sin(a3) + std::cos(a1)*std::cos(a3)*std::sin(a2);
+            this->data[3] = std::cos(a1)*std::sin(a2)*std::cos(a3) + std::sin(a1)*std::sin(a3);
             this->data[4] = std::cos(a1)*std::cos(a2);
-            this->data[5] = std::cos(a1)*std::sin(a2)*std::sin(a3) - std::cos(a3)*std::sin(a1);
-            this->data[6] = std::cos(a3)*std::sin(a1)*std::sin(a2) - std::cos(a1)*std::sin(a3);
-            this->data[7] = std::cos(a2)*std::sin(a1);
-            this->data[8] = std::cos(a1)*std::cos(a3) + std::sin(a1)*std::sin(a2)*std::sin(a3);
+            this->data[5] = std::cos(a1)*std::sin(a2)*std::sin(a3) - std::sin(a1)*std::cos(a3);
+            this->data[6] = std::sin(a1)*std::sin(a2)*std::cos(a3) - std::cos(a1)*std::sin(a3);
+            this->data[7] = std::sin(a1)*std::cos(a2);
+            this->data[8] = std::sin(a1)*std::sin(a2)*std::sin(a3) + std::cos(a1)*std::cos(a3);
         } break;
 
         case RotationSequence::ZYX_321:
         {
             this->data[0] = std::cos(a1)*std::cos(a2);
-            this->data[1] = std::cos(a1)*std::sin(a2)*std::sin(a3) - std::cos(a3)*std::sin(a1);
-            this->data[2] = std::sin(a1)*std::sin(a3) + std::cos(a1)*std::cos(a3)*std::sin(a2);
-            this->data[3] = std::cos(a2)*std::sin(a1);
-            this->data[4] = std::cos(a1)*std::cos(a3) + std::sin(a1)*std::sin(a2)*std::sin(a3);
-            this->data[5] = std::cos(a3)*std::sin(a1)*std::sin(a2) - std::cos(a1)*std::sin(a3);
+            this->data[1] = std::cos(a1)*std::sin(a2)*std::sin(a3) - std::sin(a1)*std::cos(a3);
+            this->data[2] = std::cos(a1)*std::sin(a2)*std::cos(a3) + std::sin(a1)*std::sin(a3);
+            this->data[3] = std::sin(a1)*std::cos(a2);
+            this->data[4] = std::sin(a1)*std::sin(a2)*std::sin(a3) + std::cos(a1)*std::cos(a3);
+            this->data[5] = std::sin(a1)*std::sin(a2)*std::cos(a3) - std::cos(a1)*std::sin(a3);
             this->data[6] = -std::sin(a2);
             this->data[7] = std::cos(a2)*std::sin(a3);
             this->data[8] = std::cos(a2)*std::cos(a3);
@@ -255,14 +268,14 @@ DCM<T>::DCM(const Euler<T> &e)
 
         case RotationSequence::YXZ_213:
         {
-            this->data[0] = std::cos(a1)*std::cos(a3) + std::sin(a1)*std::sin(a2)*std::sin(a3);
-            this->data[1] = std::cos(a3)*std::sin(a1)*std::sin(a2) - std::cos(a1)*std::sin(a3);
-            this->data[2] = std::cos(a2)*std::sin(a1);
+            this->data[0] = std::sin(a1)*std::sin(a2)*std::sin(a3) + std::cos(a1)*std::cos(a3);
+            this->data[1] = std::sin(a1)*std::sin(a2)*std::cos(a3) - std::cos(a1)*std::sin(a3);
+            this->data[2] = std::sin(a1)*std::cos(a2);
             this->data[3] = std::cos(a2)*std::sin(a3);
             this->data[4] = std::cos(a2)*std::cos(a3);
             this->data[5] = -std::sin(a2);
-            this->data[6] = std::cos(a1)*std::sin(a2)*std::sin(a3) - std::cos(a3)*std::sin(a1);
-            this->data[7] = std::cos(a1)*std::cos(a3)*std::sin(a2) + std::sin(a1)*std::sin(a3);
+            this->data[6] = std::cos(a1)*std::sin(a2)*std::sin(a3) - std::sin(a1)*std::cos(a3);
+            this->data[7] = std::cos(a1)*std::sin(a2)*std::cos(a3) + std::sin(a1)*std::sin(a3);
             this->data[8] = std::cos(a1)*std::cos(a2);
         } break;
 
